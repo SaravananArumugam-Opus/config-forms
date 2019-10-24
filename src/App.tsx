233e082bc07e59efe35e7600876c2613fc0887cf
Saran <saravanan.ar@protonmail.com>
2019-10-23 22:28:19 -0700

    Form component start

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,52 @@
 import React, { Component } from 'react';
-import { Button } from 'antd';
+import { Button, Form, Input } from 'antd';
 import './App.css';
+
+const formItemLayout = {
+  labelCol: { span: 4 },
+  wrapperCol: { span: 14 }
+};
+
+class _FormComponent extends React.Component<any> {
+  constructor(props: any) {
+    super(props);
+
+    this.showFormValue = this.showFormValue.bind(this);
+  }
+
+  showFormValue() {
+    console.log(this.props.form.getFieldsValue());
+  }
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <div>
+        <Form layout='horizontal'>
+          <Form.Item label='Field A' {...formItemLayout}>
+            {getFieldDecorator('fieldA', {})(
+              <Input placeholder='input placeholder' />
+            )}
+          </Form.Item>
+          <Form.Item label='Field B' {...formItemLayout}>
+            <Input placeholder='input placeholder' />
+          </Form.Item>
+          <Form.Item {...formItemLayout}>
+            <Button onClick={this.showFormValue}>Get value</Button>
+          </Form.Item>
+        </Form>
+      </div>
+    );
+  }
+}
+
+const FormComponent = Form.create()(_FormComponent);
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <Button type='primary'>Button</Button>
+        <FormComponent />
       </div>
     );
   }
